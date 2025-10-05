748c54864eb5a826e616e9162c6d839d09436e17
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-10-05 19:20:54 +0800

    Update train.py

--- train/train.py
+++ train/train.py
@@ -5,7 +5,7 @@ model = YOLO("yolo11n.pt")
 # Train the model on the COCO8 dataset for 100 epochs
 train_results = model.train(
     data="coco8.yaml",  # Path to dataset configuration file
-    epochs=2,  # Number of training epochs
+    epochs=100,  # Number of training epochs
     imgsz=640,  # Image size for training
     device="cuda",  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
 )
